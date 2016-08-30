7c417add6c037a6f527f14d2ac22df15a19de05e
Antoine <anto1ne97@users.noreply.github.com>
2016-08-30 12:57:41 +0200

    Delete usr.php

--- usr.php
+++
@@ -1,8 +0,0 @@
-<?php if (isset($_SESSION['username'])) : ?>
-	<article>
-		<header>
-			<h2>You are logged in as: <b><?php echo $_SESSION['username']; ?></b></h2>
-		</header>
-	</article>
-<?php else : ?>
-<?php endif; ?>
