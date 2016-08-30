bc19524ef24abcb5ebedc1b2200172c18d0d298c
Antoine <anto1ne97@users.noreply.github.com>
2016-08-30 12:59:00 +0200

    Add files via upload
    Don't show "You are logged in as" when user is not logged in.

--- themes/usr.php
+++ themes/usr.php
@@ -1,5 +1,8 @@
-<article>
-	<header>
-		<h2>You are logged in as: <b><?php echo $_SESSION['username']; ?></b></h2>
-	</header>
-</article>
+<?php if (isset($_SESSION['username'])) : ?>
+	<article>
+		<header>
+			<h2>You are logged in as: <b><?php echo $_SESSION['username']; ?></b></h2>
+		</header>
+	</article>
+<?php else : ?>
+<?php endif; ?>
