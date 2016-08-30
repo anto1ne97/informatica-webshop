4cb1f20867bb4a5ea6af9998084912e33badaa90
Antoine <antoine-de-groot@live.nl>
2016-08-30 23:22:08 +0200

    new files

--- themes/navbar.php
+++ themes/navbar.php
@@ -1,15 +1,15 @@
 <main>
 	<nav>
-		<h1><a href="../public_html/index.php" class="logo">Antoine de Groot</a></h1>
+		<h1><a href="../public_html/index.php" class="logo">Amanda & Co. Pannenkoeken</a></h1>
 		<ul>
 			<li><a href="../public_html/index.php">Home</a></li>
-			<li><a href="../public_html/about.php">About me</a></li>
-			<li><a href="../public_html/portfolio.php">Portfolio</a>
+			<li><a href="../public_html/about.php">Placeholder Text</a></li>
+			<li><a href="../public_html/portfolio.php">Placeholder Text</a>
 			<?php if (isset($_SESSION['username'])) : ?>
 				<ul>
-					<li><a href="../public_html/iwep.php">International Work Experience Project</a></li>
-					<li><a href="../public_html/ib.php">International Baccalaureate</a></li>
-					<li><a href="../public_html/projects.php">Other Projects</a></li>
+					<li><a href="../public_html/#.php">Placeholder Text</a></li>
+					<li><a href="../public_html/#.php">Placeholder Text</a></li>
+					<li><a href="../public_html/#.php">Placeholder Text</a></li>
 				</ul>
 			<?php endif; ?>
 			</li>
