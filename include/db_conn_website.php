52341632a435a11958ef4d8cbeddf86d9f248865
Antoine <antoine-de-groot@live.nl>
2016-10-23 21:56:06 +0200

    Delete sensitive data

--- include/db_conn_website.php
+++ include/db_conn_website.php
@@ -5,9 +5,9 @@ $errflag	= false;
 
 // configuration
 $dbhost 	= "localhost";
-$dbname		= "website";	#"taupedk35_Antoine";
-$dbuser		= "root";		#"taupedk35_DG";
-$dbpass		= "usbw";		#"Antoine";
+$dbname		= "website";
+$dbuser		= "root";
+$dbpass		= "usbw";
 $options 	= array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
 // database connection
