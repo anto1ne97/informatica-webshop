afefbae8f93cd2bf601309c1b0ccd809cb4a2ad2
Antoine <antoine-de-groot@live.nl>
2016-12-13 13:30:33 +0100

    Update database connections

--- include/register-process.php
+++ include/register-process.php
@@ -5,12 +5,12 @@ require '../include/db_conn_website.php';
 if (isset($_POST['register'])) {
 
 	// new data
-	$username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+	$email = !empty($_POST['email']) ? trim($_POST['email']) : null;
     $pass = !empty($_POST['password']) ? trim($_POST['password']) : null;
 	
 	// check whether username exists
-	$check = $conn->prepare("SELECT COUNT(username) AS num FROM users WHERE username = :username");
-	$check->bindParam(':username', $username);
+	$check = $conn->prepare("SELECT COUNT(email) AS num FROM klanten WHERE email = :email");
+	$check->bindParam(':email', $email);
 	$check->execute();
 	$rows = $check->fetch(PDO::FETCH_ASSOC);
 	if($rows['num'] > 0) {
@@ -21,8 +21,10 @@ if (isset($_POST['register'])) {
 	$passwordHash = password_hash($password, PASSWORD_BCRYPT, ["cost" => 12]);
 
 	// add user to database
-	$stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
-	$stmt->bindParam(':username', $username);
+	$stmt = $conn->prepare("INSERT INTO klanten (email, voornaam, achternaam, password) VALUES (:email, :voornaam, :achternaam, :password)");
+	$stmt->bindParam(':email', $email);
+	$stmt->bindParam(':voornaam', $voornaam);
+	$stmt->bindParam(':achternaam', $achternaam);
 	$stmt->bindParam(':password', $passwordHash);
 	$result = $stmt->execute();
 	if($result) {
