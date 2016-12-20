3b09f626fdb9f5faf60e676019fd4ee3260d1490
Antoine <antoine-de-groot@live.nl>
2016-12-20 12:45:38 +0100

    Update order page

--- public_html/about.php
+++ public_html/about.php
@@ -6,14 +6,47 @@ include_once '../themes/header.php';
 include_once '../themes/navbar.php';
 ?>
 
-<div id="header-featured">
-	<div id="banner">
-		<h2>Amanda & Co. Pannenkoeken</h2>
-		<article>
-			Welkom bij Amanda & Co pannenkoeken. Bij ons kunt u terecht als u een pannenkoek wilt eten, maar geen tijd hebt om ze te bakken! Met een simpele klik op de bestelknop kunt u de allerlekkerste pannenkoeken bestellen. Wij zijn gedreven, gastvrij, ambitieus, kundig en dienstbaar. Maar boven alles: wij hebben liefde en passie voor ons vak en stralen dat in alles wat we doen uit.<br>
-			Ons team bestaat uit een groep jonge enthousiaste medewerkers met een vakgerichte opleiding in de pannenkoeken branche onder leiding van de eigenaar Amanda meulink.<br>
-			Onze gerechten zijn gevarieerd en fijn van smaak. Het menu bestaat uit traditionele Pannenkoeken,  maar speciale pannenkoeken die alleen bij ons verkrijgbaar is. De specialiteiten zijn benoemd naar onze trouwe werknemers, die met al hun liefde proberen dit bedrijf tot een succes te brengen.  Onze koks zijn altijd bezig met het uitproberen van nieuwe gerechten om keer op keer culinaire hoogstandjes te bereiden. Bent u benieuwd naar onze gerechten? Hebben wij uw interesse al gewekt? bekijk dan snel onze menukaart!
-		</article>
+
+<div id="featured-wrapper">
+	<div id="featured" class="extra2 margin-btm container">
+		<div class="main-title">
+			<h2>Nulla eleifend purus</h2>
+			<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
+		</div>	
+		<div class="ebox1"> <span class="fa fa-pagelines"></span>
+			<div class="title">
+				<h2>Fusce ultrices fringilla</h2>
+				<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
+			</div>
+			<p>Consectetuer adipiscing elit. Nam pede erat, porta eu, lobortis eget, tempus et, tellus. Etiam neque. Vivamus consequat lorem at nisl. Nullam non wisi a sem semper eleifend. Donec mattis ibero eget urna. Duis pretium velit ac mauris. Proin eu wisi suscipit nulla suscipit interdum. Aenean lectus lorem, imperdiet at, ultrices eget, ornare et, wisi. </p>
+			<a href="#" class="button">Etiam posuere</a>
+		</div>
+		<div class="ebox2"> <span class="fa fa-anchor"></span>
+			<div class="title">
+				<h2>Donec dictum metus</h2>
+				<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
+			</div>
+			<p>Donec pulvinar ullamcorper metus. In eu odio at lectus pulvinar mollis. Vestibulum sem magna, elementum ut, vestibulum eu, facilisis quis, arcu. Mauris a dolor. Nulla facilisi. Cum sociis atoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed blandit. Phasellus pellentesque, ante nec iaculis dapibus, eros justo auctor lectus.</p>
+			<a href="#" class="button">Etiam posuere</a>
+		</div>
+	</div>
+	<div class="extra2 container">
+		<div class="ebox1"> <span class="fa fa-puzzle-piece"></span>
+			<div class="title">
+				<h2>Etiam rhoncus volutpat</h2>
+				<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
+			</div>
+			<p>Integer nisl risus, sagittis convallis, rutrum id, elementum congue, nibh. Suspendisse dictum porta lectus. Donec placerat odio vel elit. Nullam ante orci, pellentesque eget, tempus quis, ltrices in, est. Sed vel tellus. Curabitur sem urna, consequat vel, suscipit in, mattis placerat, nulla. Sed ac leo. Pellentesque imperdiet. Pellentesque viverra vulputate enim.</p>
+			<a href="#" class="button">Etiam posuere</a>
+		</div>
+		<div class="ebox2"> <span class="fa fa-comments-o"></span>
+			<div class="title">
+				<h2>Mauris vulputate dolor</h2>
+				<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
+			</div>
+			<p>Proin gravida orci porttitor enim accumsan lacinia. Donec condimentum, urna non molestie semper, ligula enim ornare nibh, quis laoreet eros quam eget ante. Aliquam libero. Vivamus nisl nibh, aculis vitae, viverra sit amet, ullamcorper vitae, turpis. Aliquam erat volutpat. Vestibulum dui sem, pulvinar sed, imperdiet nec, iaculis nec, leo. </p>
+			<a href="#" class="button">Etiam posuere</a>
+		</div>
 	</div>
 </div>
 
--- public_html/bestellen.php
+++ public_html/bestellen.php
@@ -7,55 +7,57 @@ include_once '../themes/navbar.php';
 ?>
 
 <div id="header-featured">
-	<div id="banner">
-		<h2>Amanda & Co. Pannenkoeken</h2>
-		<p>Welkom bij Amanda & Co. Pannenkoeken Restaurant. Dit is een heel gezellig restaurant op planeet Aarde in een zonnesysteem in het sterrenstelsel genaamd Melkweg. Kan iemand hier een tekst bedenken?</p>
-		<a href="/webshop/public_html/bestellen.php" class="button">Bestel nu</a>
-	</div>
-</div>
-
-<div id="wrapper">
-	<div id="featured-wrapper">
-		<div id="featured" class="extra2 margin-btm container">
-			<div class="main-title">
-				<h2>Nulla eleifend purus</h2>
-				<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
-			</div>	
-			<div class="ebox1"> <span class="fa fa-pagelines"></span>
-				<div class="title">
-					<h2>Fusce ultrices fringilla</h2>
-					<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
-				</div>
-				<p>Consectetuer adipiscing elit. Nam pede erat, porta eu, lobortis eget, tempus et, tellus. Etiam neque. Vivamus consequat lorem at nisl. Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Duis pretium velit ac mauris. Proin eu wisi suscipit nulla suscipit interdum. Aenean lectus lorem, imperdiet at, ultrices eget, ornare et, wisi. </p>
-				<a href="#" class="button">Etiam posuere</a>
-			</div>
-			<div class="ebox2"> <span class="fa fa-anchor"></span>
-				<div class="title">
-					<h2>Donec dictum metus</h2>
-					<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
-				</div>
-				<p>Donec pulvinar ullamcorper metus. In eu odio at lectus pulvinar mollis. Vestibulum sem magna, elementum ut, vestibulum eu, facilisis quis, arcu. Mauris a dolor. Nulla facilisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed blandit. Phasellus pellentesque, ante nec iaculis dapibus, eros justo auctor lectus.</p>
-				<a href="#" class="button">Etiam posuere</a>
-			</div>
+	<div id="banner" class="margin-btm bestellen">
+		<div class="main-title">
+			<h2>Stel je eigen pannekoek samen</h2>
 		</div>
-		<div class="extra2 container">
-			<div class="ebox1"> <span class="fa fa-puzzle-piece"></span>
-				<div class="title">
-					<h2>Etiam rhoncus volutpat</h2>
-					<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
+		<form method="POST" action="..include/orders.php">
+			<article class="ebox1">
+				<div >
+					<h2>Kies je bodem</h2>
 				</div>
-				<p>Integer nisl risus, sagittis convallis, rutrum id, elementum congue, nibh. Suspendisse dictum porta lectus. Donec placerat odio vel elit. Nullam ante orci, pellentesque eget, tempus quis, ultrices in, est. Sed vel tellus. Curabitur sem urna, consequat vel, suscipit in, mattis placerat, nulla. Sed ac leo. Pellentesque imperdiet. Pellentesque viverra vulputate enim.</p>
-				<a href="#" class="button">Etiam posuere</a>
-			</div>
-			<div class="ebox2"> <span class="fa fa-comments-o"></span>
-				<div class="title">
-					<h2>Mauris vulputate dolor</h2>
-					<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
+				<div>
+					<input type="checkbox">naturel</input>
+					<a href="#" class="button">button</a>
 				</div>
-				<p>Proin gravida orci porttitor enim accumsan lacinia. Donec condimentum, urna non molestie semper, ligula enim ornare nibh, quis laoreet eros quam eget ante. Aliquam libero. Vivamus nisl nibh, iaculis vitae, viverra sit amet, ullamcorper vitae, turpis. Aliquam erat volutpat. Vestibulum dui sem, pulvinar sed, imperdiet nec, iaculis nec, leo. </p>
-				<a href="#" class="button">Etiam posuere</a>
-			</div>
-		</div>
+			</article>
+			<article class="ebox1">
+				<div>
+					<h2>Kies je special</h2>
+				</div>
+				<p>
+					
+				</p>
+				<a href="#" class="button">button</a>
+			</article>
+			<article class="ebox1">
+				<div>
+					<h2>Kies je toppings</h2>
+				</div>
+				<p>
+					
+				</p>
+				<a href="#" class="button">button</a>
+			</article>
+			<article class="ebox1">
+				<div>
+					<h2>Kies je extra</h2>
+				</div>
+				<p>
+					
+				</p>
+				<a href="#" class="button">button</a>
+			</article>
+			<article class="ebox1">
+				<div>
+					<h2>Drinken</h2>
+				</div>
+				<p>
+					
+				</p>
+				<a href="#" class="button">button</a>
+			</article>
+		</form>
 	</div>
 </div>
 
