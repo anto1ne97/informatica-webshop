b73fba2de0cdee09aef7a85278691b4423a56fad
Antoine <antoine-de-groot@live.nl>
2016-12-20 21:01:42 +0100

    Improve nagbar link order

--- themes/navbar.php
+++ themes/navbar.php
@@ -6,8 +6,8 @@
 		<ul>
 <!--			<li><a href="/webshop/public_html/menukaart.php">Menukaart</a></li> -->
 			<li><a href="/webshop/public_html/bestellen.php">Bestellen</a></li>
-			<li><a href="/webshop/public_html/about.php">Wie zijn wij?</a></li>
 			<li><a href="/webshop/public_html/openingstijden.php">Openingstijden</a></li>
+			<li><a href="/webshop/public_html/about.php">Het Team</a></li>
 			<li><a href="/webshop/public_html/contact.php">Contact</a></li>
 			<?php if (isset($_SESSION['email'])) : ?>
 			<li><a href="/webshop/include/logout.php">Logout</a></li>
@@ -18,12 +18,12 @@
 	</div>
 </nav>
 <!--
-			<?php if (isset($_SESSION['email'])) : ?>
+			<?php// if (isset($_SESSION['email'])) : ?>
 				<ul>
 					<li><a href="/webshop/public_html/#.php">Placeholder Text</a></li>
 					<li><a href="/webshop/public_html/#.php">Placeholder Text</a></li>
 					<li><a href="/webshop/public_html/#.php">Placeholder Text</a></li>
 				</ul>
-			<?php endif; ?>
+			<?php// endif; ?>
 -->
 <main>
