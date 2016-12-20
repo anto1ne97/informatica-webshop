0a7663cdd3d7193f1ce7d4c0f12048a0e2c7b5bf
Antoine <antoine-de-groot@live.nl>
2016-12-20 11:57:49 +0100

    Update Session array to current account details

--- public_html/register-success.php
+++ public_html/register-success.php
@@ -8,7 +8,7 @@ include_once '../themes/header.php';
 
 <div id="header-featured">
 	<div id="banner">
-		<h2>Welkom <b><?php echo $_SESSION['username']; ?></b></h2>
+		<h2>Welkom <b><?php echo $_SESSION['email']; ?></b></h2>
 		<article>
 			U heeft met succes de registratie afgerond. Een ogenblik geduld alstublieft.
 		</article>
