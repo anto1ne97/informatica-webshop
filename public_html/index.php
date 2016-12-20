21c5d461ddc1e00dacd1fbfc7b0d0f06484d4cde
Antoine <antoine-de-groot@live.nl>
2016-12-20 10:49:28 +0100

    Fix typo

--- public_html/index.php
+++ public_html/index.php
@@ -11,8 +11,8 @@ include_once '../themes/navbar.php';
 		<h2>Amanda & Co. Pannenkoeken</h2>
 		<article>
 			Welkom bij Amanda & Co pannenkoeken. Bij ons kunt u terecht als u een pannenkoek wilt eten, maar geen tijd hebt om ze te bakken! Met een simpele klik op de bestelknop kunt u de allerlekkerste pannenkoeken bestellen. Wij zijn gedreven, gastvrij, ambitieus, kundig en dienstbaar. Maar boven alles: wij hebben liefde en passie voor ons vak en stralen dat in alles wat we doen uit.<br>
-			Ons team bestaat uit een groep jonge enthousiaste medewerkers met een vakgerichte opleiding in de pannenkoeken branche onder leiding van de eigenaar Amanda meulink.<br>
-			Onze gerechten zijn gevarieerd en fijn van smaak. Het menu bestaat uit traditionele Pannenkoeken,  maar speciale pannenkoeken die alleen bij ons verkrijgbaar is. De specialiteiten zijn benoemd naar onze trouwe werknemers, die met al hun liefde proberen dit bedrijf tot een succes te brengen.  Onze koks zijn altijd bezig met het uitproberen van nieuwe gerechten om keer op keer culinaire hoogstandjes te bereiden. Bent u benieuwd naar onze gerechten? Hebben wij uw interesse al gewekt? bekijk dan snel onze menukaart!
+			Ons team bestaat uit een groep jonge enthousiaste medewerkers met een vakgerichte opleiding in de pannenkoeken branche onder leiding van de eigenaar Amanda Meulink.<br>
+			Onze gerechten zijn gevarieerd en fijn van smaak. Het menu bestaat uit traditionele Pannenkoeken, maar speciale pannenkoeken die alleen bij ons verkrijgbaar is. De specialiteiten zijn benoemd naar onze trouwe werknemers, die met al hun liefde proberen dit bedrijf tot een succes te brengen.  Onze koks zijn altijd bezig met het uitproberen van nieuwe gerechten om keer op keer culinaire hoogstandjes te bereiden. Bent u benieuwd naar onze gerechten? Hebben wij uw interesse al gewekt? bekijk dan snel onze menukaart!
 		</article>
 		<a href="/webshop/public_html/bestellen.php" class="button">Bestel nu</a>
 	</div>
