ef62f91e76fe79af65cce0353cec8470bf7c1035
Antoine <antoine-de-groot@live.nl>
2017-01-06 00:44:25 +0100

    Change order of tags

--- themes/header.php
+++ themes/header.php
@@ -3,15 +3,17 @@
 <head>
 	<title><?=$title;?></title>
 	<meta charset="UTF-8">
-	<link rel="author" href="/webshop/humans.txt">
-	<link rel="stylesheet" href="/webshop/css/main.css">
-	<link rel="stylesheet" href="/webshop/css/fonts.css" type="text/css">
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="keywords" content="" />
 	<meta name="description" content="" />
-	<script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
-	<script src="/webshop/js/login.js"></script>
+	<link rel="author" href="/webshop/humans.txt">
+	<link rel="stylesheet" href="/webshop/css/main.css">
+	<link rel="stylesheet" href="/webshop/css/fonts.css">
+	<link rel="stylesheet" href="/webshop/css/toggle-passwords.css">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900">
+	<link rel="stylesheet" href="/css/magic-check.min.css">
+	<script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script src="/webshop/js/toggle-passwords.js"></script>
 	<script src="/webshop/js/jquery.dropotron.min.js"></script>
 	<script src="/webshop/js/jquery.scrolly.min.js"></script>
 	<script src="/webshop/js/jquery.onvisible.min.js"></script>
