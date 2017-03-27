8c0e2870d5870504e3f341cc5a841e9af58b4590
Antoine <antoine-de-groot@live.nl>
2017-03-27 19:36:33 +0200

    Edit typos

--- public_html/about.php
+++ public_html/about.php
@@ -22,14 +22,14 @@ include_once '../themes/navbar.php';
 		<div class="ebox1"> <span class="image"><img src="../images/antoinedegroot.jpg" class="image-circle"></span>
 			<div class="title">
 				<h2>Antoine de Groot</h2>
-				<span class="byline">Senior Web Developer & Chef Panenenkoekennbakker</span>
+				<span class="byline">Senior Web Developer &amp; Chef Pannenkoekenbakker</span>
 			</div>
 			<p>Ik ben Antoine en elke pannenkoek die u eet, is gemaakt en samengesteld door mij! Samen met de hulp van mijn baas, mijn leerling, de man achter de schermen en Sjoerd leveren wij zalig geluk op een bord! Hoe ik in dit bedrijf ben gerold? Oude jeugdvrienden. Amanda en ik zijn al jaren gepassioneerd over dit concept, toen zij ermee aankwam het te realiseren, kon ik deze kans niet laten schieten. en het is de beste keus die ik ooit heb gemaakt!</p>
 		</div>
 		<div class="ebox2"> <span class="image"><img src="../images/lucblankestijn.jpg" class="image-circle"></span>
 			<div class="title">
 				<h2>Luc Blankestijn</h2>
-				<span class="byline">Database Expert & Pannenkoekenbakker in opleiding</span>
+				<span class="byline">Database Expert &amp; Pannenkoekenbakker in opleiding</span>
 			</div>
 			<p>Ik ben de leerling van de beste grootste pannenkoekenbakker in ons team: Antoine. Hoe ik in dit bedrijf met mijn opleiding ben gerold? Puur geluk. Ik zat tot voor kort op de Herman Brood academie, waar Amanda de allerbesten een plekje in haar bedrijf gaf. En ik zat daar bij! Nu leer ik meer dan ooit tevoren. Ik ben dankbaar dat ik hier mag leren en werken en hoop dat wij nog lang voort zullen gaan!</p>
 		</div>
@@ -38,14 +38,14 @@ include_once '../themes/navbar.php';
 		<div class="ebox1"> <span class="image"><img src="../images/sjoerdgroneschild.jpg" class="image-circle"></span>
 			<div class="title">
 				<h2>Sjoerd Groneschild</h2>
-				<span class="byline">Web Developer & Afwasser</span>
+				<span class="byline">Web Developer &amp; Afwasser</span>
 			</div>
 			<p>Ik ben op een andere manier binnen dit bedrijf gekomen dan de andere werknemers. Ik was tot voor kort zwervende, zonder vaste verblijfplaats. Het geld wat ik van lieve mensen kreeg op straat gaf ik uit aan onnodige producten, waaronder vooral bier. Totdat Amanda mij ontdekte. Ze bood mij een baan aan in haar bedrijf en ik ben haar daarvoor dankbaar. Mijn leven is nu weer op de rails.</p>
 		</div>
 		<div class="ebox2"> <span class="image"><img src="../images/jellevanderham.jpg" class="image-circle"></span>
 			<div class="title">
 				<h2>Jelle van der Ham</h2>
-				<span class="byline">Telecommunications Specialist & Recepten maker</span>
+				<span class="byline">Telecommunications Specialist &amp; Receptenmaker</span>
 			</div>
 			<p>Ik ben de man achter de schermen die alles regelt. Ik kan geen pannenkoeken bakken, maar ben toch wel erg blij dat er ook een plekje voor mij was in dit bedrijf. Ik ga naar alle persconferenties, interviews en naar de beurs waar de allernieuwste recepten voor pannenkoeken verkocht worden. Hierdoor blijven wij ons bedrijf vernieuwen.  </p>
 		</div>
