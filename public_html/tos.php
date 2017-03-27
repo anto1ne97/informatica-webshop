9c0845e96835a8ae1c8956e26c52b24d44746ae4
Antoine <antoine-de-groot@live.nl>
2017-03-27 20:23:37 +0200

    Delete audio

--- public_html/tos.php
+++ public_html/tos.php
@@ -5,8 +5,6 @@ $title = "Gebruikersvoorwaarden";
 include_once '../themes/header.php';
 ?>
 
-<audio autoplay><source src="../audio/Thomas the Dank Engine.mp3" type="audio/mpeg"></audio>
-
 <div id="header-featured">
 	<div id="banner">
 		<h2>Amanda &amp; Co. Pannenkoeken</h2>
