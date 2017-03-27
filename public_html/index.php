767c8b8fa389be2343799fac806e5dfddfd80e81
Breax <luc.blankestijn@gmail.com>
2017-03-27 16:42:33 +0200

    Change index introduction
    Minor verandering aan de introductie tekst

--- public_html/index.php
+++ public_html/index.php
@@ -10,9 +10,9 @@ include_once '../themes/navbar.php';
 	<div id="banner">
 		<h2>Amanda &amp; Co. Pannenkoeken</h2>
 		<article>
-			Welkom bij Amanda &amp; Co pannenkoeken. Bij ons kunt u terecht als u een pannenkoek wilt eten, maar geen tijd hebt om ze te bakken! Met een simpele klik op de bestelknop kunt u de allerlekkerste pannenkoeken bestellen. Wij zijn gedreven, gastvrij, ambitieus, kundig en dienstbaar. Maar boven alles: wij hebben liefde en passie voor ons vak en stralen dat in alles wat we doen uit.<br>
+			Welkom bij Amanda &amp; Co pannenkoeken. Bij ons kunt u terecht als u een pannenkoek wilt eten, maar geen tijd hebt om ze te bakken! Met een simpele klik op de bestelknop kunt u de allerlekkerste pannenkoeken bestellen. <br> Wij zijn gedreven, gastvrij, ambitieus, kundig en dienstbaar. Maar boven alles: wij hebben liefde en passie voor ons vak en dat straalt in alles wat we doen uit.<br>
 			Ons team bestaat uit een groep jonge enthousiaste medewerkers met een vakgerichte opleiding in de pannenkoeken branche onder leiding van de eigenaar Amanda Meulink.<br>
-			Onze gerechten zijn gevarieerd en fijn van smaak. Het menu bestaat uit traditionele Pannenkoeken, maar ook speciale pannenkoeken die alleen bij ons verkrijgbaar zijn. De specialiteiten zijn genoemd naar onze trouwe werknemers, die met al hun liefde proberen dit bedrijf tot een succes te brengen. Onze chefs zijn altijd bezig met het uitproberen van nieuwe gerechten om keer op keer culinaire hoogstandjes te bereiden. Bent u benieuwd naar onze gerechten? Hebben wij uw interesse al gewekt?
+			Onze gerechten zijn gevarieerd en fijn van smaak. Het menu bestaat uit traditionele Pannenkoeken, maar ook speciale pannenkoeken die alleen bij ons verkrijgbaar zijn. De specialiteiten zijn genoemd naar onze trouwe werknemers, die met al hun liefde proberen dit bedrijf tot een succes te brengen. Onze chefs zijn altijd bezig met het uitproberen van nieuwe gerechten om keer op keer culinaire hoogstandjes te bereiden. <br> Bent u benieuwd naar onze gerechten? Hebben wij uw interesse al gewekt? Klik dan hieronder om een account aan te maken en bestel een pannenkoek!
 		</article>
 		<a href="/webshop/public_html/bestellen.php" class="button">Maak nu een account om te bestellen!</a>
 	</div>
